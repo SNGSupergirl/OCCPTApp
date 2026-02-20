d77339d57d5af02b59ccfd6074b8119040f81885
SNGSupergirl <SNGSupergirl@gmail.com>
2026-02-20 14:18:08 -0800

    Updating Join our union page

--- app/src/main/java/org/oregonccpt/occpt/ui/joinafscme/JoinAFSCMEFragment.kt
+++ app/src/main/java/org/oregonccpt/occpt/ui/joinafscme/JoinAFSCMEFragment.kt
@@ -1,9 +1,11 @@
 package org.oregonccpt.occpt.ui.joinafscme
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import org.oregonccpt.occpt.databinding.FragmentJoinAfscmeBinding
 
@@ -19,6 +21,21 @@ class JoinAFSCMEFragment : Fragment() {
     ): View {
         _binding = FragmentJoinAfscmeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val browserIntent = Intent(Intent.ACTION_VIEW, "https://findunionchildcareor.org/join-our-union".toUri())
+        binding.joinButton1.setOnClickListener {
+            startActivity(browserIntent)
+        }
+        binding.joinButton2.setOnClickListener {
+            startActivity(browserIntent)
+        }
+        binding.privacyPolicyLink.setOnClickListener {
+            val url = "https://findunionchildcareor.org/privacy-policy"
+            startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
+        }
     }
 
     override fun onDestroyView() {
