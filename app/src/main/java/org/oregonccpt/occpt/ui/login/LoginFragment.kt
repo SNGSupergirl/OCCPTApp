b02cf2050df844b1f439767514bcdef6bbab7218
SNGSupergirl <SNGSupergirl@gmail.com>
2026-02-21 01:21:33 -0800

    Working on Login Page

--- app/src/main/java/org/oregonccpt/occpt/ui/login/LoginFragment.kt
+++ app/src/main/java/org/oregonccpt/occpt/ui/login/LoginFragment.kt
@@ -1,10 +1,12 @@
 package org.oregonccpt.occpt.ui.login
 
 import android.os.Bundle
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import org.oregonccpt.occpt.databinding.FragmentLoginBinding
 
@@ -27,6 +29,15 @@ class LoginFragment : Fragment() {
 
         binding.loginButton.setOnClickListener {
             Toast.makeText(requireContext(), "Login clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.facebookIcon.setOnClickListener {
+            val url = "https://www.facebook.com/AFSCMEChildCareProviders/"
+            startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
+        }
+
+        binding.twitterIcon.setOnClickListener {
+            val url = "https://twitter.com/AFSCME"
+            startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
         }
     }
 
