ec4c8af2aacc222e7afc4acd6b6107d2ff839a9a
SNGSupergirl <SNGSupergirl@gmail.com>
2026-02-27 14:08:40 -0800

    Start of the Focus Union App-  added email, phone and add contact buttons

--- app/src/main/java/org/oregonccpt/occpt/ui/officers/OfficersAdapter.kt
+++ app/src/main/java/org/oregonccpt/occpt/ui/officers/OfficersAdapter.kt
@@ -1,8 +1,12 @@
 package org.oregonccpt.occpt.ui.officers
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import org.oregonccpt.occpt.R
@@ -25,15 +29,31 @@ class OfficersAdapter(private val officers: List<Officer>) : RecyclerView.Adapte
         if (officer.email != null) {
             holder.binding.officerEmail.text = officer.email
             holder.binding.officerEmail.visibility = View.VISIBLE
+            holder.binding.emailIcon.visibility = View.VISIBLE
+            holder.binding.emailIcon.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:${officer.email}")
+                }
+                ContextCompat.startActivity(holder.itemView.context, intent, null)
+            }
         } else {
             holder.binding.officerEmail.visibility = View.GONE
+            holder.binding.emailIcon.visibility = View.GONE
         }
 
         if (officer.phone != null) {
             holder.binding.officerPhone.text = officer.phone
             holder.binding.officerPhone.visibility = View.VISIBLE
+            holder.binding.callIcon.visibility = View.VISIBLE
+            holder.binding.callIcon.setOnClickListener {
+                val intent = Intent(Intent.ACTION_DIAL).apply {
+                    data = Uri.parse("tel:${officer.phone}")
+                }
+                ContextCompat.startActivity(holder.itemView.context, intent, null)
+            }
         } else {
             holder.binding.officerPhone.visibility = View.GONE
+            holder.binding.callIcon.visibility = View.GONE
         }
 
         if (officer.photo != 0) {
@@ -42,6 +62,21 @@ class OfficersAdapter(private val officers: List<Officer>) : RecyclerView.Adapte
             }
         } else {
             holder.binding.officerPhoto.setImageResource(R.drawable.ic_profile_placeholder)
+        }
+
+        if (officer.email != null || officer.phone != null) {
+            holder.binding.addContactIcon.visibility = View.VISIBLE
+            holder.binding.addContactIcon.setOnClickListener {
+                val intent = Intent(ContactsContract.Intents.Insert.ACTION).apply {
+                    type = ContactsContract.RawContacts.CONTENT_TYPE
+                    putExtra(ContactsContract.Intents.Insert.NAME, officer.name)
+                    putExtra(ContactsContract.Intents.Insert.EMAIL, officer.email)
+                    putExtra(ContactsContract.Intents.Insert.PHONE, officer.phone)
+                }
+                ContextCompat.startActivity(holder.itemView.context, intent, null)
+            }
+        } else {
+            holder.binding.addContactIcon.visibility = View.GONE
         }
     }
 
