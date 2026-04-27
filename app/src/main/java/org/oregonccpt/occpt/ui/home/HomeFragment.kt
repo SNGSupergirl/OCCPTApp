7913a27963d5f67fe4a14ba34cbad6560ed1f9d2
SNGSupergirl <SNGSupergirl@gmail.com>
2026-04-27 11:21:12 -0700

    Updating to PILLARS

--- app/src/main/java/org/oregonccpt/occpt/ui/home/HomeFragment.kt
+++ app/src/main/java/org/oregonccpt/occpt/ui/home/HomeFragment.kt
@@ -1,36 +1,21 @@
 package org.oregonccpt.occpt.ui.home
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.tabs.TabLayoutMediator
 import org.oregonccpt.occpt.R
 import org.oregonccpt.occpt.databinding.FragmentHomeBinding
-import java.util.Timer
-import java.util.TimerTask
+import androidx.core.view.isGone
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private lateinit var slideshowAdapter: SlideshowAdapter
-    private val images = listOf(
-        R.drawable.home_2019_december_mtg_training,
-        R.drawable.home_2020_april_capitol_protest,
-        R.drawable.home_2020_april_saturday_kates_house,
-        R.drawable.home_2018_retreat_class,
-        R.drawable.home_2018_bargaining_team
-    )
-    private var currentPage = 0
-    private var timer: Timer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,15 +24,8 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        slideshowAdapter = SlideshowAdapter(images)
-        binding.viewPager.adapter = slideshowAdapter
-
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { _, _ -> }.attach()
-
-        createSlideShow()
-
         binding.facebookIcon.setOnClickListener {
-            val url = "https://www.facebook.com/AFSCMEChildCareProviders/"
+            val url = "https://www.facebook.com/PILLARSofOregon/"
             startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
         }
 
@@ -58,18 +36,13 @@ class HomeFragment : Fragment() {
         binding.joinUnionButton.setOnClickListener {
             val url = "https://findunionchildcareor.org/sign-up-page/"
             val intent = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse(url)
+                data = url.toUri()
             }
             startActivity(intent)
         }
 
-        binding.unionlyButton.setOnClickListener {
-            val url = "https://unionly.io/o/oregon-afscme-ccpt-local-132/store/products"
-            startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
-        }
-
         binding.ourUnionContainer.setOnClickListener {
-            if (binding.ourUnionExpandableLayout.visibility == View.GONE) {
+            if (binding.ourUnionExpandableLayout.isGone) {
                 binding.ourUnionExpandableLayout.visibility = View.VISIBLE
                 binding.clickMoreInfoLayout.visibility = View.INVISIBLE
             } else {
@@ -81,26 +54,8 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-    private fun createSlideShow() {
-        val handler = Handler(Looper.getMainLooper())
-        val update = Runnable {
-            if (currentPage == images.size) {
-                currentPage = 0
-            }
-            binding.viewPager.setCurrentItem(currentPage++, true)
-        }
-
-        timer = Timer()
-        timer?.schedule(object : TimerTask() {
-            override fun run() {
-                handler.post(update)
-            }
-        }, 3000, 3000)
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
-        timer?.cancel()
         _binding = null
     }
 }
